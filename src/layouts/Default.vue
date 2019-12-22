8ae8fb89b2530642b956972583e57e28ab5dcbc9
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2019-12-22 21:36:37 +0200

    Initial commir with tailwind & purgecss

---
+++ src/layouts/Default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="layout">
+    <header class="bg-yellow-brand p-4">
+      <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong>
+      <nav class="nav">
+        <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/about/">About</g-link>
+      </nav>
+    </header>
+    
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+@import url('https://rsms.me/inter/inter.css');
+html { font-family: 'Inter', sans-serif; }
+@supports (font-variation-settings: normal) {
+  html { font-family: 'Inter var', sans-serif; }
+}
+
+</style>
