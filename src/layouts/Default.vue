3ca289abf0c4840cf5ee1e7fefd0d785c3b13481
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2019-12-23 07:35:36 +0200

    Add header and portfolio section

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,66 @@
 <template>
   <div class="layout">
     <header class="bg-yellow-brand p-4">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+      <nav class="container mx-auto flex items-center justify-between">
+        <div>
+          <a href="#" class="nav__link">Portfolio</a>
+          <a href="#" class="nav__link" to="#">Services</a>
+          <a href="#" class="nav__link" to="#">Services</a>
+        </div>
+
+        <div class='bg-white p-4 rounded-lg shadow w-24 mr-8'>
+          <g-image src="~/progic-logo.svg" class="w-full" />
+          <span class="text-gray-800 text-center block">Founder</span>
+        </div>
+
+        <div>
+          <a href="#" class="nav__link">Twitter</a>
+          <a href="#" class="nav__link" to="#">Github</a>
+          <a href="#" class="nav__link" to="#">Blog</a>
+        </div>
       </nav>
+
+
+      <div class="text-center my-24 ">
+        <h1 class="text-5xl font-black">It's Kudakwashe Paradzayi</h1>
+        <p class="text-lg">A very brief but concise description about the value I can provide to clients</p>
+      </div>
+
+      <div class="text-center mb-48">
+        <button class="py-2 px-4 bg-black text-white text-lg rounded shadow uppercase">call to action</button>
+      </div>
+
     </header>
-    
+
+    <!-- Higglight the app of the month -->
+
+    <section class="flex justify-center">
+      <div class="w-full max-w-xl absolute z-50 -mt-32">
+        <div class="text-right -mb-4 mr-4">
+            <span class="inline-block bg-yellow-brand shadow border-2 border-black py-1 px-4 rounded">App Of The Month</span>
+        </div>
+
+        <div class="flex flex-col bg-white w-full shadow-lg rounded p-4">
+          <!-- the maximize, resize and close buttons-->
+          <div class="flex mb-4">
+            <div class="h-4 w-4 rounded-full bg-red-400 mr-2">&nbsp;</div>
+            <div class="h-4 w-4 rounded-full bg-yellow-400 mr-2">&nbsp;</div>
+            <div class="h-4 w-4 rounded-full bg-green-400">&nbsp;</div>
+          </div>
+
+          <!-- The image of the  -->
+          <div class="w-full flex-1  mb-4">&nbsp;
+            <g-image src="~/highlight-procurement.png"/>
+          </div>
+
+          <!-- Visit the applicatioin -->
+
+          <button class="border-2 border-black text-lg w-full p-4">
+            Visit Project
+          </button>
+        </div>
+      </div>
+    </section>  
     <slot/>
   </div>
 </template>
@@ -27,6 +78,10 @@ query {
 html { font-family: 'Inter', sans-serif; }
 @supports (font-variation-settings: normal) {
   html { font-family: 'Inter var', sans-serif; }
+}
+
+.nav__link {
+  @apply mr-8 font-medium text-xl
 }
 
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,45 @@
 <template>
   <Layout>
+    <section class="bg-black text-white pt-64 pb-32 relative">
+      <div class="pt-64 container mx-auto">
+        <div class="flex">
+          <g-image src="~/dotted-trianlge.svg" class="mr-4 -ml-Hello kuda32"/>
+          <div class="">
+            <h1 class="text-5xl font-black">Portfolio</h1>
+            <p class="text-lg">See what I have created in the past and enjoy yourself!</p>
+          </div>
+        </div>
 
 
+        <div class="mt-32 flex justify-between -mx-16 flex-wrap" >
+          <div class="px-16 py-2 w-1/2 gallery-image" v-for="i in 4" :key="i">
+            <div class="bg-white p-4 relative" @mouseenter="showDescriptionFor(i)" @mouseleave="hideDescription" >
+              <div :class="{hidden: shownDescription !== i }" class=" absolute flex flex-col justify-between center-absolute bg-transparent-yellow-brand text-black">
+                <div class="p-4">
+                  <h2 class="text-3xl  font-medium mb-4">Prazlion - Procurement System</h2>
+                  <p class="text-lg">Internal app capable of doing X,Y and Z.</p>
+                </div>
+
+                <button class="bg-black text-white text-lg w-full p-4">
+                  Visit Project
+                </button>
+              </div>
+              <!-- The image of the  -->
+              <div class="w-full flex-1  mb-4">&nbsp;
+                <g-image src="~/highlight-procurement.png"/>
+              </div>
+            </div>
+            <!-- Visit the applicatioin -->
+          </div>
+          
+          <div class="px-16 gallery-image">
+            <button class="border-2 border-white text-white text-lg  p-4">
+              See more of my work &rightarrow;
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 </template>
 
@@ -9,6 +47,22 @@
 export default {
   metaInfo: {
     title: 'Hello, world!'
+  },
+
+  data () {
+    return {
+      shownDescription: null
+    }
+  },
+
+  methods: {
+    showDescriptionFor(i) {
+      this.shownDescription = i
+    },
+
+    hideDescription() {
+      this.shownDescription = null
+    }
   }
 }
 </script>
@@ -16,5 +70,22 @@ export default {
 <style>
 .home-links a {
   margin-right: 1rem;
+}
+
+.gallery-image:nth-child(even) {
+  @apply mt-16;
+}
+
+.gallery-image:nth-child(odd) {
+  @apply -mt-16;
+}
+
+.bg-transparent-yellow-brand {
+  background-image: linear-gradient(#fdd231, #fdd2310a)
+}
+
+.center-absolute {
+  width: calc(100% - 2rem);
+  height: calc(100% - 2rem);
 }
 </style>
