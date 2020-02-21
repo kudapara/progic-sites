e1458094bbf98ad40c0f84b5d8ccaeee9e5fca56
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2020-02-21 23:16:32 +0200

    hide the checbox when accordion is open

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -58,8 +58,11 @@
 }
 .tab input[type="radio"]:checked + label::after {
   transform: rotateX(180deg);
-  background-color: #000; /*.bg-indigo*/
+  background-color: #fff; /*.bg-indigo*/
+  border-color: #fff;
   color: #f8fafc; /*.text-grey-lightest*/
+  display: hidden;
+  /* hide the toggle from plain sight */
 }
 </style>
 <template>
@@ -72,8 +75,12 @@
         <div 
           v-for="(item, i) in items" :key="i"
           class="tab w-full overflow-hidden border-t bg-white text-black mb-8">
-          <input class="absolute opacity-0" id="tab-single-one" type="radio" name="tabs2" />
-          <label class="block p-5 leading-normal cursor-pointer font-bold" for="tab-single-one">{{ item.title }}</label>
+
+          <input class="absolute opacity-0"
+           v-model="openFAQ" :value="i" :id="`tab-single-${i}`" type="radio" name="tabs2" />
+          <label class="block p-5 leading-normal cursor-pointer font-bold" :for="`tab-single-${i}`">{{ item.title }}
+            
+          </label>
           <div
             class="tab-content overflow-hidden border-l-8 bg-gray-100 border-yellow-brand leading-normal"
           >
@@ -91,21 +98,27 @@
 
    <script>
 /* Optional Javascript to close the radio button version by clicking it again */
-var myRadios = document.getElementsByName("tabs2");
-var setCheck;
-var x = 0;
-for (x = 0; x < myRadios.length; x++) {
-  myRadios[x].onclick = function() {
-    if (setCheck != this) {
-      setCheck = this;
-    } else {
-      this.checked = false;
-      setCheck = null;
-    }
-  };
-}
+// var myRadios = document.getElementsByName("tabs2");
+// var setCheck;
+// var x = 0;
+// for (x = 0; x < myRadios.length; x++) {
+//   myRadios[x].onclick = function() {
+//     alert('checked')
+//     if (setCheck != this) {
+//       setCheck = this;
+//     } else {
+//       this.checked = false;
+//       setCheck = null;
+//     }
+//   };
+// }
 export default {
   props: ['items', 'title'],
+  data () {
+    return {
+      openFAQ: 0
+    }
+  },
   methods: {
         paragraphExtractor (text) {
       return text
