34c4e26ed2fc8b9d382b8e40498395f6368e5ae3
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2021-01-15 11:00:13 +0200

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -35,20 +35,6 @@ query {
 }
 </static-query>
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/600154c6a9a34e36b96caea6/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-
 <style>
 /* @import url('https://rsms.me/inter/inter.css'); */
 @import url('https://fonts.googleapis.com/css2?family=Inter&family=Sansita:wght@400;900&display=swap');
