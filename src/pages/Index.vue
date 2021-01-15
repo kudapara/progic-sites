7da722554970eb236b954535b63a65c11a3461aa
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2021-01-15 11:05:03 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -391,6 +391,19 @@
       title: "Progic.co | Premium designs for premium brands"
     },
 
+    mounted() {
+      <!--Start of Tawk.to Script-->
+      var window.Tawk_API=window.Tawk_API||{}, window.Tawk_LoadStart=new Date();
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/600154c6a9a34e36b96caea6/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+
+      <!--End of Tawk.to Script-->
+    },
+    
     components: {
       Accordion,
       VueMailchimpEmailSignupForm
